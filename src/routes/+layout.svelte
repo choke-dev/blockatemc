bd3f44d453bd43817194b765920b6572b85e3e32
choke-dev <admin@choke.dev>
2024-12-30 00:40:09 +0800

    refactor: anchor dot pattern background to viewport

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,8 @@
 </script>
 
 <div class="flex flex-col min-h-screen">
-	<div class="absolute inset-0 h-full w-full bg-[radial-gradient(#241C1C_1px,transparent_1px)] [background-size:16px_16px] -z-50"></div> <!-- optimally, i would want this to calculate the color based on the background with a lighter shade, but i dont know how, if you're reading this and know how, please let me know -->
+	<div class="fixed inset-0 h-full w-full bg-[radial-gradient(#241C1C_1px,transparent_1px)] [background-size:16px_16px] -z-50"></div>
+ <!-- optimally, i would want this to calculate the color based on the background with a lighter shade, but i dont know how, if you're reading this and know how, please let me know -->
 	<div class="fixed top-0 inset-x-0 hidden md:block">
 		<Header />
 	</div>
