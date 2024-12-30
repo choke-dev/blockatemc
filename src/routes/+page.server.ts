c29498d7c4f2c158e579a24786d3245053255c50
choke-dev <admin@choke.dev>
2024-12-30 08:25:49 +0800

    refactor: improve site loading times

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { MINECRAFT_SERVER_IP } from "$lib/config";
 import type { PageServerLoad } from "./$types";
 import mc from "minecraftstatuspinger";
 
-const retryRequest = async <T>(fn: () => Promise<T>, retries = 3, initialDelay = 100): Promise<T | undefined> => {
+const retryRequest = async <T>(fn: () => Promise<T>, retries = 3, initialDelay = 0): Promise<T | undefined> => {
     let delay = initialDelay;
 
     for (let attempt = 1; attempt <= retries; attempt++) {
@@ -12,18 +12,18 @@ const retryRequest = async <T>(fn: () => Promise<T>, retries = 3, initialDelay =
             if ( (error instanceof Error) && error.message.includes('Timed out')) return;
             if (attempt >= retries) return undefined;
 
-            console.warn(`Attempt ${attempt} failed. Retrying in ${delay}ms...`);
-            await new Promise((resolve) => setTimeout(resolve, delay));
+            // console.warn(`Attempt ${attempt} failed. Retrying in ${delay}ms...`);
+            // await new Promise((resolve) => setTimeout(resolve, delay));
             
-            const savedDelay = delay;
-            delay = savedDelay * 2;
+            // const savedDelay = delay;
+            // delay = savedDelay * 2;
         }
     }
 };
 
 const getServerInfo = async (): Promise<any> => {
     const fetchServerInfo = async (): Promise<any> => mc.lookup({ hostname: MINECRAFT_SERVER_IP });
-    const data = await retryRequest(fetchServerInfo, 3, 1000);
+    const data = await retryRequest(fetchServerInfo);
     return data?.status;
 };
 
